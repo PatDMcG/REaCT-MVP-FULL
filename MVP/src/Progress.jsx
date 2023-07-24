33f19c569ebba25b888d647b4abfc69224883fbd
PatDMcG <110838464+PatDMcG@users.noreply.github.com>
2023-07-24 17:15:20 -0400

    css

--- MVP/src/Progress.jsx
+++ MVP/src/Progress.jsx
@@ -30,8 +30,8 @@ function Progress({subList, master}) {
           <div style={{ width: 150, marginRight: 550}}>
             <CircularProgressbar value={totalPercent.current } text={`${totalPercent.current }%`} 
             styles={buildStyles({ pathColor: `rgba(62, 152, 199, ${totalPercent.current / 100})`,
-    textColor: 'slategray',
-    trailColor: 'slateblue',
+    textColor: 'darkgray',
+    trailColor: 'darkblue',
     backgroundColor: '#3e98c7'})}/>
           </div>
         </div>
