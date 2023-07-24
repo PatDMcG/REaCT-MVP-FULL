be023b417d607196b327c5f5bfd5342d096222c3
PatDMcG <110838464+PatDMcG@users.noreply.github.com>
2023-07-24 13:08:07 -0400

    test again

--- MVP/src/NewGoal.jsx
+++ MVP/src/NewGoal.jsx
@@ -14,19 +14,19 @@ function NewGoal() {
           
        
         event.preventDefault()
-        // let resp = await fetch("https://react-mvp-full.onrender.com/goals/main",
-        // {
-        //     headers: {
-        //       'Accept': 'application/json',
-        //       'Content-Type': 'application/json'
-        //     },
-        //     method: "POST",
-        //     body: JSON.stringify({title: `${data}`})
-        // })
-        // let result = await resp.json()
+        let resp = await fetch("https://react-mvp-full.onrender.com/goals/main",
+        {
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify({title: `${data}`})
+        })
+        let result = await resp.json()
        
         //setData("test")
-        console.log(data)
+        console.log(result)
       }
        catch (error) {
         console.log(error)
