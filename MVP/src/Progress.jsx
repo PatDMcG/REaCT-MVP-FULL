99d6108976dc7d1da74e1f4ebfc8956c86aa810e
PatDMcG <110838464+PatDMcG@users.noreply.github.com>
2023-07-24 08:07:04 -0400

    progressbar

--- MVP/src/Progress.jsx
+++ MVP/src/Progress.jsx
@@ -1,13 +1,36 @@
-//import { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
-function Progress() {
+
+function Progress({SubList}) {
+    let totaldone = 0;
+    let total = 0
+    for(let i = 0; i < SubList.length; i++)
+    {
+        total++
+        if(sublist[i].complete)
+        {
+            totaldone++
+        }
+    }
+      const [percentage, setPercentage] = useState(0);
     
-    return (
-        <>
-        <h1>CSS HERE</h1>
-        </>
-    )
-}
+      useEffect(() => {
+          if (percentage < 100) {
+            setPercentage(totaldone/total);
+          }
+      }, [percentage]);
+    
+      return (
+        <div style={{textAlign:"center"}}>
+          <h4>Circular progress bar in React </h4>
+          <div style={{ width: 150, marginLeft: 550}}>
+            <CircularProgressbar value={percentage} text={`${percentage}%`} />
+          </div>
+        </div>
+      );
+    }
 
 
 export default Progress
